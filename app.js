97a161afcaa667ead7625b2ff45f394eb416dce1
Ryan Bozarth <ryan.bozarth@gmail.com>
2016-11-06 12:48:02 -0800

    add bartender questions

--- app.js
+++ app.js
@@ -1,79 +1,138 @@
 $(function() {
 
-Welcome();
-function Welcome() {
-  $('.btn_welcome').on('click', function(event){
-    event.preventDefault();
-    var CustomerName = $('#customer-name').val();
-    console.log(CustomerName);
-  $('.intro').remove();
-  });
-}
+var Question = function(question, property){
+  this.question = question;
+  this.property = property;
+};
 
-function CheckName(CustomerName) {
-
-}
-
-function Customers(name, drink, food) {
-  this.name = name;
-  this.drink = drink;
-  this.food = food;
-}
-
-function Ingredient(name, type) {
-  this.name = name;
+function Ingredient(name, type){
   this.type = type;
+  this.name = name;
 }
 
 function Pantry(name){
   this.name = name;
-  this.contents = {
-    strong: [],
-    salty: [],
-    bitter:[],
-    sweet: [],
-    fruit: []
-  };
+  this.contents = {};
 }
 
-Pantry.prototype.addIngredient = function(ingredient) {
-  if (!this.contents[ingredient.type]) {
-    this.contents[ingredient.type] = [ingredient.name];
-  } else {
+Pantry.prototype.addIngredient = function(ingredient){
+  if (this.contents[ingredient.type]){
     this.contents[ingredient.type].push(ingredient.name);
+  } else {
+    this.contents[ingredient.type] = [ingredient.name];
   }
 }
 
-//
-// var Questions = [{
-//   prompt: 'Do ye like yer drinks strong?',
-//   key: 'strong',
-//   answers: ['yes', 'no']
-// },
-// {
-//   prompt: 'Do ye like it with a salty tang?',
-//   key: 'salt',
-//   answers: ['yes', 'no']
-// },
-// {
-//   prompt: 'Are ye a lubber who likes it bitter?',
-//   key: 'bitter',
-//   answers: ['yes', 'no']
-// },
-// {
-//   prompt: 'Would ye like a bit of sweetness with yer poison?',
-//   key: 'sweet',
-//   answers: ['yes', 'no']
-// },
-// {
-//   prompt: 'Are ye one for a fruity finish?',
-//   key: 'fruit',
-//   answers: ['yes', 'no']
-// }]
-// var Worker = [{
-//   name: 'Bartender'
-// }]
-//
+Pantry.prototype.getAllIngredients = function(type){
+  return this.contents[type];
+}
+
+Pantry.prototype.getIngredient = function(type){
+  var numIngredients = this.contents = this.contents[type].length;
+  var random = Math.floor(Math.random() * numIngredients);
+  return this.contents[type][random];
+}
+
+var Worker = function(name){
+  this.name = name;
+  this.questions = []
+  this.customers = [];
+}
+
+Worker.prototype.who = function(){
+    alert("My name is " + this.name);
+}
+
+Worker.prototype.greet = function(){
+  var name = prompt("So, what's your name?");
+  if(this.custoomers[name]){
+    alert ("Welcome back " + name);
+    alert("Here is your favorite" + this.customers[name].favorite)
+  } else {
+    var newCustomer = Customer(name);
+    this.customers[name] == newCustomer;
+  }
+}
+
+var Bartender = function (name){
+  Worker.call(this.name);
+  this.questions = []
+}
+
+Bartender.prototype = Object.create(Worker.prototype);
+Bartender.prototype.constructor = Bartender;
+
+Bartender.prototype.askQuestions = function(question){
+  this.questions.push(question)
+}
+var bar = new Bartender('bar');
+// var chef = new Chef('chef');
+// var mypantry = new mypantry('pantry');
+
+// Pantry.addIngredient(new Ingredient('sugar', 'sweet'));
+// Pantry.addIngredient(new Ingredient('salt', 'salty'));
+// Pantry.addIngredient(new Ingredient('agave', 'sweet'));
+
+function addQuestion(question){
+  this.questions.push(question)
+}
+
+var newQuestion = new Question('Do you like yer drinks strong?', 'strong')
+addQuestion(newQuestion);
+var newQuestion = new Question('Do ye like it with a salty tang?', 'salty')
+addQuestion(newQuestion);
+var newQuestion = new Question('Are ye a lubber who likes it bitter?', 'bitter')
+addQuestion(newQuestion);
+var newQuestion = new Question('Would ye like a bit of sweetness with yer poison?', 'sweet')
+addQuestion(newQuestion);
+var newQuestion = new Question('Are ye one for a fruity finish?', 'fruity')
+addQuestion(newQuestion);
+
+
+var Customer = function(name, drink){
+  this.name = name;
+  this.favorite = drink;
+}
+
+var Drink = function(name, ingredients){
+  // array of the ingredients object
+}
+
+$('.order-drink').click(function(){
+  worker = "bar";
+  // can I use just Bartender above
+  $('.intro').addClass("hidden");
+  $('.enter-name').removeClass("hidden");
+    // bar.asksQuestions();
+});
+
+nameForm();
+function nameForm() {
+  $('.btn_name').on('click', function(event){
+    event.preventDefault();
+    var customerName = $('#customer-name').val();
+    console.log(customerName);
+    if(customerName.length === 0){
+      alert("Please enter a name!")
+    };
+    $('.enter-name').addClass('hidden');
+    askQuestions();
+  });
+};
+  // hide after name submitted
+
+  function askQuestions(){
+      var html = "";
+      for (var i=0; i < this.questions.length; i++)  {
+      html += this.questions[i].question;
+      html += "<input type='checkbox' class='option' name='preference'";
+      html += "value='" + this.questions[i].property + "'<br/><br/>";
+      }
+      html += "<input type='submit' class='btn btn-preferences' value='Submit Order'>";
+      $('.questions-form').html(html);
+      console.log(questions);
+  };
+
 // init();
 // function init(){
 //   BartenderAsks.push(Questions);
@@ -91,16 +150,9 @@ Pantry.prototype.addIngredient = function(ingredient) {
 //   });
 //
 // };
-// function askQuestion() {
-//   for (BartenderAsks = 0; BartenderAsks < Questions.length; i++)
-//     $('.questions').removeClass('hidden').append(Questions[BartenderAsks].prompt).append(Questions[BartenderAsks].answers);
-//     // add one to questions counter
-// }
-// // function AskQuestions(){
-// //     for(var i=0; i < BartenderAsks.length; i++)  {
-// //     // push questions into the question array
-// //     // // }
-// // };
+
+
+
 
 
 
